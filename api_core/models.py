86333acfc4a67f1ef49548b276f2b9b5cc476f2f
mvilchis <mvilchis@ciencias.unam.mx>
2017-06-26 13:45:56 -0500

    Add save user and

--- api_core/models.py
+++ api_core/models.py
@@ -6,51 +6,43 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.conf import settings
-
-HORROR = 'horror'
-COMEDY = 'comedy'
-ACTION = 'action'
-DRAMA = 'drama'
-
-CATEGORIES_CHOICES = ((HORROR, 'Horror'), (COMEDY, 'Comedy'),
-                      (ACTION, 'Action'), (DRAMA, 'Drama'), )
-
+from django.contrib.auth.models import User
 MAN = 'hombre'
 WOMAN = 'mujer'
 
 SEX_CHOICES = ((WOMAN, 'mujer'), (MAN, 'hombre'), )
 
 Aguascalientes = 'Aguascalientes'
-Baja_California = 'Baja California'
+Baja_California = 'Baja_California'
 Baja_California_Sur = 'Baja_California_Sur'
 Campeche = 'Campeche'
 Chiapas = 'Chiapas'
 Chihuahua = 'Chihuahua'
-Ciudad_de_Mexico = 'Ciudad de México'
+Ciudad_de_Mexico = 'Ciudad_de_Mexico'
 Coahuila = 'Coahuila'
 Colima = 'Colima'
 Durango = 'Durango'
-Estado_de_Mexico = 'Estado de México'
+Estado_de_Mexico = 'Estado_de_Mexico'
 Guanajuato = 'Guanajuato'
 Guerrero = 'Guerrero'
 Hidalgo = 'Hidalgo'
 Jalisco = 'Jalisco'
-Michoacan = 'Michoacán'
+Michoacan = 'Michoacan'
 Morelos = 'Morelos'
 Nayarit = 'Nayarit'
-Nuevo_Leon = 'Nuevo León'
+Nuevo_Leon = 'Nuevo_Leon'
 Oaxaca = 'Oaxaca'
 Puebla = 'Puebla'
-Queretaro = 'Querétaro'
-Quintana_Roo = 'Quintana Roo'
-San_Luis_Potosi = 'San Luis Potosí'
+Queretaro = 'Queretaro'
+Quintana_Roo = 'Quintana_Roo'
+San_Luis_Potosi = 'San_Luis_Potosi'
 Sinaloa = 'Sinaloa'
 Sonora = 'Sonora'
 Tabasco = 'Tabasco'
 Tamaulipas = 'Tamaulipas'
 Tlaxcala = 'Tlaxcala'
 Veracruz = 'Veracruz'
-Yucatan = 'Yucatán'
+Yucatan = 'Yucatan'
 Zacatecas = 'Zacatecas'
 
 COUNTRY_CHOICES = (
@@ -78,61 +70,61 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
 class Project(models.Model):
     class Meta:
         abstract = True
-
+    user = models.ForeignKey(User, null = True)
     ID_Proyecto = models.IntegerField()
     ID_Usuario = models.IntegerField()
-    Categoria = models.CharField(max_length=10, choices=CATEGORIES_CHOICES)
-    Subcategoria = models.CharField(max_length=10, choices=CATEGORIES_CHOICES)
+    Categoria = models.CharField(max_length=10, blank=True, null=True)
+    Subcategoria = models.CharField(max_length=10,  blank=True, null=True)
 
 
 class ProjectDonation(Project):
-    Plazo_Dias = models.IntegerField()
+    Plazo_Dias = models.IntegerField( blank=True, null=True)
     Fecha_Inicio = models.DateField()
-    Fecha_Termino = models.DateField()
+    Fecha_Termino = models.DateField( blank=True, null=True)
     Monto_Recaudado = models.FloatField()
 
 
 class ProjectRecompesas(Project):
-    Plazo_Dias = models.IntegerField()
+    Plazo_Dias = models.IntegerField( blank=True, null=True)
     Fecha_Inicio = models.DateField()
-    Fecha_Termino = models.DateField()
+    Fecha_Termino = models.DateField( blank=True, null=True)
     Monto_Recaudado = models.FloatField()
 
 
 class ProjectCapital(Project):
     Monto_Aprobado = models.FloatField()
-    Plazo_Meses = models.IntegerField()
-    Tasa_Interes = models.FloatField()
-    Fecha_Inicio = models.DateField()
-    Fecha_Termino = models.DateField()
     Monto_Recaudado = models.FloatField()
-    Cuota_Comision = models.FloatField()
-    Fondeado = models.BooleanField()
+    Monto_Fondeado = models.BooleanField( blank=True)
+    Fecha_Inicio = models.DateField()
+    Fecha_Termino = models.DateField( blank=True, null=True)
+    Plazo_Dias = models.IntegerField( blank=True, null=True)
+    Tasa_Interes = models.FloatField( blank=True, null=True)
+    Cuota_Comision = models.FloatField( blank=True, null=True)
 
 
 class ProjectDeuda(Project):
     Monto_Aprobado = models.FloatField()
-    Monto_Fondeado = models.FloatField()
-    Plazo_Meses = models.IntegerField()
-    Tasa_Anual_fraccion = models.FloatField()
-    Fecha_Aprobado = models.DateField()
-    Fecha_Liberado = models.DateField()
-    Comision_Apertura = models.FloatField()
-    Capital_Pagado = models.FloatField()
-    Interes_Pagados = models.FloatField()
-    Interes_Moratorios_Pagados = models.FloatField()
+    Monto_Fondeado = models.FloatField( blank=True, null=True)
+    Plazo_Dias = models.IntegerField( blank=True, null=True)
+    Tasa_Anual_fraccion = models.FloatField( blank=True, null=True)
+    Fecha_Termino = models.DateField( blank=True, null=True)
+    Fecha_Inicio = models.DateField()
+    Comision_Apertura = models.FloatField( blank=True, null=True)
+    Capital_Pagado = models.FloatField( blank=True, null=True)
+    Interes_Pagados = models.FloatField( blank=True, null=True)
+    Interes_Moratorios_Pagados = models.FloatField( blank=True, null=True)
 
 
 class UserProject(models.Model):
     class Meta:
         abstract = True
 
+    user = models.ForeignKey(User, null =True)
     ID_usuario = models.IntegerField()
-    Sexo = models.CharField(max_length=10, choices=SEX_CHOICES)
-    AnioNacimiento = models.DateField()
-    Estado = models.CharField(max_length=10, choices=COUNTRY_CHOICES)
-
-    Edad = models.IntegerField()
+    Sexo = models.CharField(max_length=10, choices=SEX_CHOICES, blank=True, null=True)
+    AnioNacimiento = models.DateField( blank=True, null=True)
+    Estado = models.CharField(max_length=10, choices=COUNTRY_CHOICES, blank=True, null=True)
+    Edad = models.IntegerField( blank=True, null=True)
 
 
 class UserDonation(UserProject):
@@ -140,7 +132,7 @@ class UserDonation(UserProject):
 
 
 class UserRecompensas(UserProject):
-    Codigo_Postal = models.IntegerField()
+    Codigo_Postal = models.IntegerField( blank=True, null=True)
 
 
 class UserCapital(UserProject):
@@ -148,14 +140,14 @@ class UserCapital(UserProject):
 
 
 class UserDeuda(UserProject):
-    Estado_Civil = models.CharField(max_length=10)
-    Codigo_Postal = models.IntegerField()
+    Estado_Civil = models.CharField(max_length=10, blank=True, null=True)
+    Codigo_Postal = models.IntegerField( blank=True, null=True)
 
 
 class Funding(models.Model):
     class Meta:
         abstract = True
-
+    user = models.ForeignKey(User,null = True)
     ID_Proyecto = models.IntegerField()
     ID_Usuario = models.IntegerField()
     Monto_Recaudado = models.FloatField()
@@ -167,8 +159,8 @@ class FundingDonation(Funding):
 
 
 class FundingRecompensas(Funding):
-    Fondeadores = models.IntegerField()
-    Exito_Fondeo = models.BooleanField()
+    Fondeadores = models.IntegerField( blank=True, null=True)
+    Exito_Fondeo = models.BooleanField(blank=True)
 
 
 class FundingCapital(Funding):
@@ -176,4 +168,4 @@ class FundingCapital(Funding):
 
 
 class FundingDeuda(Funding):
-    Tipo_Movimiento = models.CharField(max_length=10)
+    Tipo_Movimiento = models.CharField(max_length=10, blank=True, null=True)
--- api_core/serializer.py
+++ api_core/serializer.py
@@ -1,78 +1,96 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField,CurrentUserDefault
 
 from models import ProjectDonation, UserDonation, FundingDonation
 from models import ProjectRecompesas, UserRecompensas, FundingRecompensas
 from models import ProjectCapital, UserCapital, FundingCapital
 from models import ProjectDeuda, UserDeuda, FundingDeuda
-
+from rest_framework.fields import CurrentUserDefault
 
 class ProjectDonationSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
     class Meta:
         model = ProjectDonation
         exclude = ('id', )
 
 
 class UserDonationSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = UserDonation
         exclude = ('id', )
 
-
 class FundingDonationSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = FundingDonation
         exclude = ('id', )
 
 
 class ProjectRecompesasSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = ProjectRecompesas
         exclude = ('id', )
 
 
 class UserRecompensasSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = UserDonation
         exclude = ('id', )
 
 
 class FundingRecompensasSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = FundingDonation
         exclude = ('id', )
 
 
 class ProjectCapitalSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = ProjectCapital
         exclude = ('id', )
 
-
 class UserCapitalSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = UserCapital
         exclude = ('id', )
 
-
 class FundingCapitalSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = FundingCapital
         exclude = ('id', )
 
 
 class ProjectDeudaSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = ProjectDeuda
         exclude = ('id', )
 
 
 class UserDeudaSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
     class Meta:
         model = UserDeuda
         exclude = ('id', )
 
-
 class FundingDeudaSerializer(ModelSerializer):
+    user = PrimaryKeyRelatedField(read_only=True, default=CurrentUserDefault())
+
     class Meta:
         model = FundingDeuda
         exclude = ('id', )
