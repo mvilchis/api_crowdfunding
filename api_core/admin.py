42a9cffa53c2fd775b6b3b5c2c2db8106c0079d6
mvilchis <mvilchis@ciencias.unam.mx>
2017-07-31 12:31:15 -0500

    Change admin view

--- api_core/admin.py
+++ api_core/admin.py
@@ -5,7 +5,23 @@ from django.contrib import admin
 from .models import  *
 # Register your models here.
 
-admin.site.register(ProjectDonation)
-admin.site.register(ProjectDeuda)
-admin.site.register(ProjectRecompesas)
-admin.site.register(ProjectCapital)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ID_Proyecto', 'ID_usuario')
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user','ID_usuario', 'Estado')
+class FundingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ID_Proyecto', 'ID_Usuario')
+
+
+admin.site.register(ProjectDonation, ProjectAdmin)
+admin.site.register(ProjectDeuda, ProjectAdmin)
+admin.site.register(ProjectRecompesas, ProjectAdmin)
+admin.site.register(ProjectCapital,ProjectAdmin)
+admin.site.register(UserDonation,UserAdmin)
+admin.site.register(UserRecompensas, UserAdmin)
+admin.site.register(UserDeuda,UserAdmin)
+admin.site.register(UserCapital,UserAdmin)
+admin.site.register(FundingDonation,FundingAdmin)
+admin.site.register(FundingCapital,FundingAdmin)
+admin.site.register(FundingDeuda,FundingAdmin)
+admin.site.register(FundingRecompensas,FundingAdmin)
