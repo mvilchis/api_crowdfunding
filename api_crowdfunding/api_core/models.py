7e361b7629557aace8aa74a1b5ee105144eba7fa
Ismael <ismaelfm_@ciencias.unam.mx>
2017-10-08 17:40:29 -0500

    Formatting and isort yapf

--- api_crowdfunding/api_core/models.py
+++ api_crowdfunding/api_core/models.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-from django.conf import settings
-from django.contrib.auth.models import User
 
-###################    MY Libraries    ###################
 from .states import *
+
 MAN = 'hombre'
 WOMAN = 'mujer'
 
-SEX_CHOICES = (("M", 'H'), ("F", 'F'), ("Masculino","M"), ("Femenino","F"),
-               ('Hombre',"H"), ("Mujer","F") )
+SEX_CHOICES = (("M", 'H'), ("F", 'F'), ("Masculino", "M"), ("Femenino", "F"),
+               ('Hombre', "H"), ("Mujer", "F"))
 
-ESTADO_CIVIL = (("Soltero", "soltero"), ("Casado","casado"), ("casado", "casado"), ("soltero", "soltero"))
+ESTADO_CIVIL = (("Soltero", "soltero"), ("Casado", "casado"),
+                ("casado", "casado"), ("soltero", "soltero"))
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
@@ -46,17 +48,22 @@ class Project(models.Model):
     #    unique_together = (("user", "ID_Proyecto", "ID_Usuario",
     #                        "Fecha_Inicio", "Monto_Recaudado"), )
 
+
 class ProjectDonation(Project):
     pass
+
 
 class ProjectRecompesas(Project):
     pass
 
+
 class ProjectCapital(Project):
     pass
 
+
 class ProjectDeuda(Project):
     pass
+
 
 class UserProject(models.Model):
     class Meta:
@@ -75,21 +82,27 @@ class UserProject(models.Model):
     #class Meta:
     #    unique_together = (("user", "ID_usuario"), )
 
+
 class UserDonation(UserProject):
     pass
+
 
 class UserRecompensas(UserProject):
     pass
 
+
 class UserCapital(UserProject):
     pass
+
 
 class UserDeuda(UserProject):
     pass
 
+
 class Funding(models.Model):
     class Meta:
         abstract = True
+
     user = models.ForeignKey(User, null=True)
     IdProyecto = models.IntegerField()
     IdUsuario = models.IntegerField()
@@ -102,11 +115,14 @@ class Funding(models.Model):
 class FundingDonation(Funding):
     pass
 
+
 class FundingRecompensas(Funding):
     pass
 
+
 class FundingCapital(Funding):
     pass
+
 
 class FundingDeuda(Funding):
     pass
