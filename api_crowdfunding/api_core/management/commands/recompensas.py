c5870520e7003c3f5f3d2bd5e078f5b4e69775f9
Ismael <ismaelfm_@ciencias.unam.mx>
2017-10-12 22:09:54 -0500

    Fixing recompensas (mi cochinito issues)

--- api_crowdfunding/api_core/management/commands/recompensas.py
+++ api_crowdfunding/api_core/management/commands/recompensas.py
@@ -1,10 +1,11 @@
+import pandas as pd
 from pandas.core.groupby import GroupBy
 
-from api_core.indexes import (PROJECTS_CATEGORIES, format_funding_data,
-                              format_projects_data, format_users_data,
-                              get_acumulado, get_fundind_data, get_indexes,
-                              get_projects_data, get_users_data, merge_data,
-                              nunique, save_data)
+from api_core.indexes import (FUNDINGS_CATEGORIES, PROJECTS_CATEGORIES,
+                              format_funding_data, format_projects_data,
+                              format_users_data, get_acumulado,
+                              get_fundind_data, get_indexes, get_projects_data,
+                              get_users_data, merge_data, nunique, save_data)
 from api_core.mi_cochinito import (get_micochinito_donations,
                                    get_micochinito_donors,
                                    get_micochinito_projects)
@@ -19,24 +20,28 @@ class Command(BaseCommand):
     help = "Help command message"
 
     def handle(self, **options):
+        projects_data_aux = get_micochinito_projects()
+        donors = projects_data_aux[[u'Fondeadores', u'IdProyecto']]
         projects_data = format_projects_data(
-            pd.concat([
-                get_projects_data(ProjectRecompesas),
-                get_micochinito_projects()
-            ]))
+            pd.concat(
+                [get_projects_data(ProjectRecompesas), projects_data_aux]))
         users_data = format_users_data(
             pd.concat(
                 [get_users_data(UserRecompensas),
                  get_micochinito_donors()]))
+        fundings_data_aux = pd.merge(
+            get_micochinito_donations(),
+            donors,
+            how='left',
+            on=[u'IdProyecto'])
         fundings_data = format_funding_data(
-            pd.concat([
-                get_fundind_data(FundingRecompensas),
-                get_micochinito_donations()
-            ]))
+            pd.concat(
+                [get_fundind_data(FundingRecompensas), fundings_data_aux]))
+        projects_users_data, fundings_users_data = merge_data(
+            projects_data, users_data, fundings_data)
 
         succesful_projects_data = projects_users_data.loc[projects_users_data[
             u'ExitoFondeo'] == True]
-
         indexes = [{
             'dataframe': projects_users_data,
             'operation': GroupBy.size,
@@ -61,9 +66,9 @@ class Command(BaseCommand):
             'elements': PROJECTS_CATEGORIES[:-2],
             'column': u'ExitoFondeo'
         }, {
-            'dataframe': projects_users_data,
+            'dataframe': fundings_users_data,
             'operation': GroupBy.sum,
-            'elements': PROJECTS_CATEGORIES[:-2],
+            'elements': FUNDINGS_CATEGORIES,
             'column': u'Fondeadores'
         }, {
             'dataframe': projects_users_data,
